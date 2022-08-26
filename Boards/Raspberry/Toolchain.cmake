f9d0fea57c8230a0dc3bdc667b065049a7964460
Andreas Dirmeier <coolcow_ccos@yahoo.com>
2022-08-26 14:39:34 +0200

    Initial import from CcOS

---
+++ Boards/Raspberry/Toolchain.cmake
@@ -0,0 +1,2 @@
+message("- Crosscompiling Toolchain for Raspberry activated")
+include(${CMAKE_CURRENT_SOURCE_DIR}/../../gcc-linaro-arm-linux-gnueabihf/Toolchain.cmake)
---
+++ Configs/Config.Generic.cmake
@@ -0,0 +1,1 @@
+set(GENERIC TRUE)
