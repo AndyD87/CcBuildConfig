fcbf9e1db894e12a1b875bba9d3c9478f0826939
Andreas Dirmeier <coolcow_ccos@yahoo.com>
2022-08-31 10:14:29 +0200

    Build CcOS in a third party dir if set

--- Find/FindCcOS.cmake
+++ Find/FindCcOS.cmake
@@ -1,9 +1,15 @@
 # Download the latest CcOS Release
-if(NOT EXISTS ${CMAKE_CURRENT_LIST_DIR}/CcOS)
-  CcGitClone(${CMAKE_CURRENT_LIST_DIR}/CcOS "https://coolcow.de/projects/CcOS.git")
+if(THIRDPARTY_DIR)
+  set(CACHE_CCOS_DIR ${THIRDPARTY_DIR}/CcOS)
+else()
+  set(CACHE_CCOS_DIR ${CMAKE_CURRENT_LIST_DIR}/CcOS)
 endif()
 
-add_subdirectory(${CMAKE_CURRENT_LIST_DIR}/CcOS/Sources)
+if(NOT EXISTS ${CACHE_CCOS_DIR})
+  CcGitClone(${CACHE_CCOS_DIR} "https://coolcow.de/projects/CcOS.git")
+endif()
+
+add_subdirectory(${CACHE_CCOS_DIR}/Sources)
 
 foreach(comp ${CcOS_FIND_COMPONENTS})
   if(NOT TARGET CcOS::${comp})
